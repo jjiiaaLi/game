1823226f6e302b0728a67481852545361189c4bb
Jack <fighterbee87@gmail.com>
2021-09-09 14:54:46 -0700

    pong

---
+++ game.py
@@ -0,0 +1,80 @@
+import turtle
+
+wndw = turtle.Screen()
+wndw.title('Jack\'s first game')
+wndw.bgcolor('white')
+wndw.setup(width=800, height=600)
+wndw.tracer(0)
+
+# paddle A
+paddle_a = turtle.Turtle()
+paddle_a.speed(0) #update speed to the fasted setting
+paddle_a.shape('square')
+paddle_a.color('black')
+paddle_a.shapesize(stretch_wid=4, stretch_len=0.5)
+paddle_a.penup()  #turtle default to drawing a line to movement and we dont want that
+paddle_a.goto(-350,0)
+
+#paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)  # update speed to the fasted setting
+paddle_b.shape('square')
+paddle_b.color('black')
+paddle_b.shapesize(stretch_wid=4, stretch_len=0.5)
+paddle_b.penup()  # turtle default to drawing a line to movement and we dont want that
+paddle_b.goto(350, 0)
+
+
+#ball
+ball = turtle.Turtle()
+ball.speed(0)  # update speed to the fasted setting
+ball.shape('circle')
+ball.color('black')
+ball.penup()  # turtle default to drawing a line to movement and we dont want that
+ball.goto(0, 0)
+ball.x_speed = 2  #ball movement speed in x axis
+ball.y_speed = 2  #ball movement speed in y axis
+
+# Move paddle functions
+def paddle_a_up():
+    current_y_coord = paddle_a.ycor()
+    current_y_coord += 10
+    paddle_a.sety(current_y_coord)
+
+def paddle_a_down():
+    current_y_coord = paddle_a.ycor()
+    current_y_coord -= 10
+    paddle_a.sety(current_y_coord)
+
+
+def paddle_b_up():
+    current_y_coord = paddle_b.ycor()
+    current_y_coord += 10
+    paddle_b.sety(current_y_coord)
+
+
+def paddle_b_down():
+    current_y_coord = paddle_b.ycor()
+    current_y_coord -= 10
+    paddle_b.sety(current_y_coord)
+
+# keyBind
+
+wndw.listen()
+wndw.onkeypress(paddle_a_up,"w")
+wndw.onkeypress(paddle_a_down,'s')
+wndw.onkeypress(paddle_b_up,'Up')
+wndw.onkeypress(paddle_b_down,'Down')
+
+
+
+
+
+
+
+# Main game loop
+while True:
+    wndw.update()
+
+    #move the ball
+
