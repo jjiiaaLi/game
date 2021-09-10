4ca2a110f776f254507efe2005a9e45949a3d123
Jack <fighterbee87@gmail.com>
2021-09-09 19:23:36 -0700

    enemy squid added

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ pygame.init()
 
 #initialize game window size
 display_window = pygame.display.set_mode((800, 600))
-
+clock = pygame.time.Clock()
 
 # Title and icon
 pygame.display.set_caption('Python Invaders')
@@ -17,11 +17,19 @@ pygame.display.set_icon(ramen_icon)
 player_img = pygame.image.load('./images/player.png')
 player_x = 370  # player initial position x axis
 player_y = 480  # player initial position y axis
+player_delta_x = 0
 
 def player(x,y):  #  function to be called to place player on the screen
-    display_window.blit(player_img,(x,yy))
+    display_window.blit(player_img,(x,y))
 
 
+#enemy
+enemy_img = pygame.image.load('./images/enemy.png')
+enemy_x = 0
+enemy_y = 0
+
+def enemy(x,y):
+    display_window.blit(enemy_img,(x,y))
 
 # Game loop
 running = True
@@ -35,10 +43,20 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-
+                player_delta_x = -3
             if event.key == pygame.K_RIGHT:
-        
-            
-    
+                player_delta_x = 3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                player_delta_x = 0
+
+    clock.tick(60)
+
     player(player_x,player_y) # render the player image
+    player_x += player_delta_x
+
+    enemy(enemy_x,enemy_y)
+
+    if player_x < 0 or player_x > 738:
+        player_delta_x = 0
     pygame.display.update()
