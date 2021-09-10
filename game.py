3ee74d17194f5ff7449f1432244803cf70f14f5a
Jack <fighterbee87@gmail.com>
2021-09-09 20:52:53 -0700

    laser shoots

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # initialize the pygame module
 pygame.init()
@@ -6,6 +7,10 @@ pygame.init()
 #initialize game window size
 display_window = pygame.display.set_mode((800, 600))
 clock = pygame.time.Clock()
+
+#background image
+background_img = pygame.image.load('./images/background.png')
+
 
 # Title and icon
 pygame.display.set_caption('Python Invaders')
@@ -25,19 +30,34 @@ def player(x,y):  #  function to be called to place player on the screen
 
 #enemy
 enemy_img = pygame.image.load('./images/enemy.png')
-enemy_x = 0
-enemy_y = 0
+enemy_x = 600
+enemy_y = 40
+enemy_delta_x = 2
 
 def enemy(x,y):
     display_window.blit(enemy_img,(x,y))
+
+# laser
+laser_img = pygame.image.load('./images/laser.png')
+laser_x = 0
+laser_y = 0
+laser_y_delta = -9
+laser_state = 'ready'
+
+def fire_laser(x,y):
+    global laser_state
+    laser_state = 'fire'
+    display_window.blit(laser_img,( x + 18, y + 19 ))
+
 
 # Game loop
 running = True
 
 while running:
     display_window.fill((0, 0, 0))  # draw the screen background
+    display_window.blit(background_img,(0,0))
 
-
+    clock.tick(60)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -49,14 +69,34 @@ while running:
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player_delta_x = 0
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                laser_state = 'fire'
+                laser_x = player_x
+                laser_y = player_y
 
-    clock.tick(60)
+    
 
     player(player_x,player_y) # render the player image
     player_x += player_delta_x
 
-    enemy(enemy_x,enemy_y)
-
+    #player border limit
     if player_x < 0 or player_x > 738:
         player_delta_x = 0
+
+    #enemy initial enter
+    enemy(enemy_x, enemy_y)
+    enemy_x += enemy_delta_x
+    if enemy_x >740 or enemy_x < 20:
+        enemy_delta_x *= -1
+        enemy_y += 50
+
+    if laser_state == 'fire':
+        fire_laser(laser_x,laser_y)
+        laser_y += laser_y_delta
+        
+    if laser_y < 0:
+        laser_state = 'ready'
+
+
     pygame.display.update()
