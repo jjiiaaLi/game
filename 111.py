bc0be1b65e950ebcf50b2a10c63f4defb6bd2d91
Jack <fighterbee87@gmail.com>
2021-09-26 10:13:23 -0700

    sight change

--- 111.py
+++ 111.py
@@ -7,11 +7,16 @@ clock = pygame.time.Clock()
 
 #objects
 sight_img = pygame.image.load('./images/sight.png')
-background = pygame.image.load('./images/background.jpg')
+background_img = pygame.image.load('./images/background.jpg')
+
+def background(x,y):
+    screen.blit(background_img,(x,y))
+background_x = -900
+background_x_delta = 0
 background_y = -4080
-background_y_delta = .5
+background_y_delta = 0.7
 def sight():
-    screen.blit(sight_img,(270,0))
+    screen.blit(sight_img,(0,0))
 
 bomb_state = 'ready'  # place holder for space command
 
@@ -21,18 +26,33 @@ running = True
 while running:
 
     screen.fill((255,255,255))
-    screen.blit(background,(0,background_y))
-    background_y += background_y_delta
-    clock.tick(60)
-
-    sight()
+    
+    clock.tick(30)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        if event.type == pygame.KEYDOWN:
+
+            if event.key == pygame.K_LEFT:
+                background_x_delta = 0.6
+            if event.key == pygame.K_RIGHT:
+                background_x_delta = -0.6
+            
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                backgroud_x_delta = 0
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bomb_state = 'pickle'
-    
+
+    background(background_x,background_y)
+    background_x += background_x_delta
+    background_y += background_y_delta
+
+    sight()
+
 
     pygame.display.update()
