269419429b49dcb00323ae880ec0a6febba75b86
Jack <fighterbee87@gmail.com>
2021-09-26 16:33:00 -0700

    impact on aimed pickle point

--- 111.py
+++ 111.py
@@ -8,18 +8,28 @@ clock = pygame.time.Clock()
 #objects
 sight_img = pygame.image.load('./images/sight.png')
 background_img = pygame.image.load('./images/background.jpg')
-ramen_img = pygame.image.load('./images/ramen.png')
+blast_img = pygame.image.load('./images/blast.png')
+
+def blast(x,y):
+    screen.blit(blast_img,(x,y))
+
+blast_x = 650
+blast_y = 400
+blast_y_delta = 0.7
+
+count_to_blast = 640
 
 def background(x,y):
     screen.blit(background_img,(x,y))
 background_x = -900
 background_x_delta = 0
-background_y = -4080
+background_y = -2080
 background_y_delta = 0.7
 def sight():
     screen.blit(sight_img,(0,0))
 
 bomb_state = 'ready'  # place holder for space command
+bomb_sight_rotate_count = 0
 
 def ramen():
     screen.blit(ramen_img,(0,0))
@@ -50,6 +60,7 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
                 bomb_state = 'pickle'
+                bomb_sight_rotate_count = 25
 
     background(background_x,background_y)
     background_x += background_x_delta
@@ -57,5 +68,15 @@ while running:
 
     sight()
 
-
+    if bomb_state == 'pickle': 
+        if bomb_sight_rotate_count > 0:
+            background_y_delta = -15
+            bomb_sight_rotate_count -= 1
+        if bomb_sight_rotate_count == 0:
+            background_y_delta = 0.7
+        if count_to_blast >0:
+            count_to_blast -= 1
+    if count_to_blast == 0:
+        blast(blast_x,blast_y)
+        blast_y += blast_y_delta
     pygame.display.update()
