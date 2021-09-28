ba9563493667859c67e5fe24e061b74d4dcaef3b
Jack <fighterbee87@gmail.com>
2021-09-27 17:42:20 -0700

    commit

--- 111.py
+++ 111.py
@@ -8,53 +8,27 @@ clock = pygame.time.Clock()
 #objects
 sight_img = pygame.image.load('./images/sight.png')
 background_img = pygame.image.load('./images/background.jpg')
-blast_img = pygame.image.load('./images/blast.png')
+exp1_img = pygame.image.load('./images/exp/exp1.png')
+exp2_img = pygame.image.load('./images/exp/exp2.png')
+exp3_img = pygame.image.load('./images/exp/exp3.png')
+exp4_img = pygame.image.load('./images/exp/exp4.png')
+exp5_img = pygame.image.load('./images/exp/exp5.png')
+exp6_img = pygame.image.load('./images/exp/exp6.png')
 
-class Explosion(pygame.sprite.Sprite):
-    def __init__(self,x,y):
-        pygame.sprite.Sprite.__init__(self)
-        self.images = []
-        for num in range(1,6):
-            img = pygame.image.load(f'./images/explosion/exp{num}.png')
-            #scale with img = pygame.transform.scale(img,(100,100))
-            self.images.append(img)
-        self.index = 0
-        self.image = self.images[self.index]
-        self.rect = self.image.get_rect()
-        self.rect.center = [x,y]
-        self.counter = 0
 
-        
 
-    
-    def update(self):
-        explosion_speed = 4
-        #update explosion animation
-        self.counter += 1
 
-        if self.counter >= explosion_speed and self.index < len(self.images) -1:
-            self.counter = 0
-            self.index += 1
-            self.image = self.images[self.index]
-
-        #if animation is complete, reset animation index
-        if self.index >= len(self.images) - 1 and self.counter >= explosion_speed:
-            self.kill()
-
-def blast(x,y):
-    screen.blit(blast_img,(x,y))
-
-blast_x = 650
-blast_y = 400
+blast_x = 640
+blast_y = 370
 blast_y_delta = 0.7
 
-count_to_blast = 640
+count_to_blast = 600
 
 def background(x,y):
     screen.blit(background_img,(x,y))
 background_x = -900
 background_x_delta = 0
-background_y = -1080
+background_y = -3900
 background_y_delta = 0.7
 def sight():
     screen.blit(sight_img,(0,0))
@@ -98,8 +72,7 @@ while running:
     background_x += background_x_delta
     background_y += background_y_delta
 
-    explosion_group.draw(screen)
-    explosion_group.update()
+    
 
     sight()
 
@@ -109,9 +82,21 @@ while running:
             bomb_sight_rotate_count -= 1
         if bomb_sight_rotate_count == 0:
             background_y_delta = 0.7
-        if count_to_blast >0:
-            count_to_blast -= 1
-    if count_to_blast == 0:
-        blast(blast_x,blast_y)
-        blast_y += blast_y_delta
+        if count_to_blast < 0 and count_to_blast >-12:
+            screen.blit(exp1_img,(blast_x,blast_y))
+            blast_y += blast_y_delta
+        elif count_to_blast < -12 and count_to_blast > -14:
+            screen.blit(exp4_img, (blast_x, blast_y))
+            blast_y += blast_y_delta
+        elif count_to_blast < -14 and count_to_blast > -16:
+            screen.blit(exp5_img, (blast_x, blast_y))
+            blast_y += blast_y_delta
+        elif count_to_blast < -16 :
+            screen.blit(exp6_img, (blast_x, blast_y))
+            blast_y += blast_y_delta
+        count_to_blast -= 1
+   
+
+
+
     pygame.display.update()
